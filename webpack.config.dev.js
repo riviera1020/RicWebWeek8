d330a0f59925bdab49ba817a1031e662fb2dbe24
riviera1020 <riviera1020@gmail.com>
2016-05-01 14:25:39 +0800

    first

---
+++ webpack.config.dev.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client', // 這多一個 hot reload 的 code entry
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/, // 針對 css 檔
+      loaders: ['style', 'css'],
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/ // 不要處理 3rd party 的 code
+    }]
+  }
+};
